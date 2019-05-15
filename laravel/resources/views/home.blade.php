25cad76e6a9e8d5597ba754ce2f60f1b6e27d7af
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2019-05-15 05:33:33 -0400

    sorry

---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.index')
+
+@section('contenido')
+    <div class="row pt-3">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    This is some text within a card block.
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
